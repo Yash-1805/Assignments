41cbaa34fd894f163cdc8a2f4c70f9dfb2b2eb90
Yash-1805 <yashraman365@gmail.com>
2025-03-30 02:38:48 +0530

    commited

---
+++ Assignment-1/task1.py
@@ -0,0 +1,8 @@
+def maths(x,y):
+    print("Addition :",x+y)
+    print("Subtraction :",x-y)
+    print("Multiplication :",x*y)
+    print("Division :",x/y)
+a= int(input("Enter the first number :"))
+b= int(input("Enter the second number :"))
+maths(a,b)
